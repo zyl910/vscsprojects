3689ac7988e1cfc7a87d2209c0e693c914daa240
zyl910 <zyl910@sina.com>
2017-05-15 11:33:57 +0800

    * `ClassLibrary1`: `Smartphone`, Allow unsafe. `C:\VS2005\SmartDevices\SDK\CompactFramework\2.0\v1.0\WindowsCE\mscorlib.dll`

---
+++ vs08_2005/SmartDevice/Smartphone/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+#region Using directives
+
+using System;
+using System.Text;
+
+#endregion
+
+namespace ClassLibrary1 {
+	public class Class1 {
+		public Class1() {
+
+		}
+	}
+}
---
+++ vs08_2005/SmartDevice/Smartphone/ClassLibrary1/ClassLibrary1/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//
+// 有关程序集的常规信息通过下列属性集
+// 控制。更改这些属性值可修改
+// 与程序集关联的信息。
+//
+[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyCopyright("版权所有 (C) Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("eaf37ab7-e11f-400e-b9d9-58f17b795605")]
+
+//
+// 程序集的版本信息由下面四个值组成:
+//
+//      主版本
+//      次版本
+//      内部版本号
+//      修订号
+//
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
+// 方法是按如下所示使用“*”:
+
+[assembly: AssemblyVersion("1.0.*")]
