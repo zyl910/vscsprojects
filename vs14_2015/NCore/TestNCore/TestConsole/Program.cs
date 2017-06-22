3680310862a78acefd8da3bf257f7c3e989692d2
zyl910 <zyl910@sina.com>
2017-06-22 20:53:13 +0800

    vs14_2015\NCore\TestNCore: TestConsole

---
+++ vs14_2015/NCore/TestNCore/TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using LibNStd10;
+using LibPcl259;
+using LibShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole {
+	class Program {
+		static void Main(string[] args) {
+			const string myproject = "TestConsole";
+			StringBuilder sb = new StringBuilder();
+			LibSharedUtil.OutputHead(sb, myproject);
+			LibNStd10Util.OutputInfo(sb, myproject);
+			LibPcl259Util.OutputInfo(sb, myproject);
+			// show.
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
