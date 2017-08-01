4fc55d53857c03d694edcfcc58b35fb8d21edfcf
zyl910 <zyl910@sina.com>
2017-08-01 18:04:16 +0800

    vs14_2015\Xamarin\App_Xamarin_Shared

---
+++ vs14_2015/Xamarin/App_Xamarin_Shared/App_Xamarin_Shared/App_Xamarin_Shared.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App_Xamarin_Shared.Droid
+{
+	[Activity (Label = "App_Xamarin_Shared", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new App_Xamarin_Shared.App ());
+		}
+	}
+}
+
