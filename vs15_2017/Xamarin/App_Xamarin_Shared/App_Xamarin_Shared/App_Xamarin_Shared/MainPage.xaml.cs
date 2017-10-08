fcc93361572cd33120bf96ee27aa0dc5f277b7fc
zyl910 <zyl910@sina.com>
2017-10-08 21:32:58 +0800

    App_Xamarin_Shared: Test btnInfo_Click

--- vs15_2017/Xamarin/App_Xamarin_Shared/App_Xamarin_Shared/App_Xamarin_Shared/MainPage.xaml.cs
+++ vs15_2017/Xamarin/App_Xamarin_Shared/App_Xamarin_Shared/App_Xamarin_Shared/MainPage.xaml.cs
@@ -5,13 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App_Xamarin_Shared
-{
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
+namespace App_Xamarin_Shared {
+	public partial class MainPage : ContentPage {
+		public MainPage() {
 			InitializeComponent();
+			InitializeBind();
+		}
+
+		public void InitializeBind() {
+			//Debug.WriteLine("InitializeBind");
+			Button btnInfo = this.FindByName<Button>("btnInfo");
+			if (null != btnInfo) {
+				btnInfo.Clicked += btnInfo_Click;
+			}
+		}
+
+		private void btnInfo_Click(object sender, EventArgs e) {
+			//Debug.WriteLine("btnInfo_Click");
+			StringBuilder sb = new StringBuilder();
+			// test.
+			sb.AppendLine("App_Xamarin_Pcl");
+			sb.AppendLine();
+			// test.
+			sb.AppendLine("[Device]");
+			sb.AppendLine(string.Format("Idiom: {0}", Device.Idiom));
+#pragma warning disable 0618
+			sb.AppendLine(string.Format("OS: {0}", Device.OS));
+#pragma warning restore 0618
+			sb.AppendLine(string.Format("RuntimePlatform: {0}", Device.RuntimePlatform));
+			// done.
+			Editor txtInfo = this.FindByName<Editor>("txtInfo");
+			txtInfo.Text = sb.ToString();
 		}
 	}
 }
