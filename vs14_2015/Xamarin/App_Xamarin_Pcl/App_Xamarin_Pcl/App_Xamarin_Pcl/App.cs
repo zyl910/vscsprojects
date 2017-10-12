a37cc221176b88e46aad1719b47f5b4c4a60de51
zyl910 <zyl910@sina.com>
2017-10-12 17:45:12 +0800

    bs2015/App_Xamarin_Shared: Test btnInfo_Click

--- vs14_2015/Xamarin/App_Xamarin_Pcl/App_Xamarin_Pcl/App_Xamarin_Pcl/App.cs
+++ vs14_2015/Xamarin/App_Xamarin_Pcl/App_Xamarin_Pcl/App_Xamarin_Pcl/App.cs
@@ -8,21 +8,22 @@ using Xamarin.Forms;
 namespace App_Xamarin_Pcl {
 	public class App : Application {
 		public App() {
-			// The root page of your application
-			var content = new ContentPage {
-				Title = "App_Xamarin_Pcl",
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+			//// The root page of your application
+			//var content = new ContentPage {
+			//	Title = "App_Xamarin_Pcl",
+			//	Content = new StackLayout {
+			//		VerticalOptions = LayoutOptions.Center,
+			//		Children = {
+			//			new Label {
+			//				HorizontalTextAlignment = TextAlignment.Center,
+			//				Text = "Welcome to Xamarin Forms!"
+			//			}
+			//		}
+			//	}
+			//};
 
-			MainPage = new NavigationPage(content);
+			//MainPage = new NavigationPage(content);
+			MainPage = new App_Xamarin_Pcl.MainPage();
 		}
 
 		protected override void OnStart() {
