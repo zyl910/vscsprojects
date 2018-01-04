2f1da8752b26d034467cff5e142e860a9e5a9b72
zyl910 <zyl910@sina.com>
2018-01-04 18:08:41 +0800

    vs14_2015\NCore\ReflectSpeed: Add ReflectSpeedNCore project.

--- vs14_2015/NCore/ReflectSpeed/ReflectSpeedNCore/Program.cs
+++ vs14_2015/NCore/ReflectSpeed/ReflectSpeedNCore/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿using ReflectSpeed;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReflectSpeed {
+namespace ReflectSpeedNCore {
 	/// <summary>
-	/// 反射速度测试的程序 - .NET Framework.
+	/// 反射速度测试的程序 - .NET Core.
 	/// </summary>
-	class Program {
-		static void Main(string[] args) {
+	public class Program {
+		public static void Main(string[] args) {
 			StringBuilder sb = new StringBuilder();
-			sb.AppendLine("ReflectSpeed - .NET Framework 4.6");
+			sb.AppendLine("ReflectSpeed - .NET Core 1.0");
 			sb.AppendLine();
 			ReflectSpeedTester tester = new ReflectSpeedTester();
 			tester.Run(sb);
