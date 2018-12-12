26004046350933bc871a7db465ab4b010eee39b9
zyl910 <zyl910@sina.com>
2018-12-12 15:11:33 +0800

    WpfLocalizationDemo: Add ApplicationResources . 初步实现绑定显示资源文本, 但发现绑定内容没刷新, 总是显示中文内容

---
+++ vs10_2010/Windows/WpfLocalizationDemo/WpfLocalizationDemo/ApplicationResources.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using WpfLocalizationDemo.Properties;
+
+namespace WpfLocalizationDemo {
+	/// <summary>
+	/// WPF应用程序级资源. 对应的资源文件是 Labels.resx .
+	/// </summary>
+	public class ApplicationResources : INotifyPropertyChanged {
+		private static ApplicationResources current;
+		private static Labels lables;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public ApplicationResources() {
+			current = this;	// 自动绑定最后一个创建的对象.
+			lables = new Labels();
+		}
+
+		public static ApplicationResources Current {
+			get { return ApplicationResources.current; }
+			set { ApplicationResources.current = value; }
+		}
+
+		public static Labels Labels {
+			get { return ApplicationResources.lables; }
+			set { ApplicationResources.lables = value; }
+		}
+
+		protected virtual void RaisePropertyChanged(string propertyName = null) {
+			var handler = this.PropertyChanged;
+			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void ChangeCulture(System.Globalization.CultureInfo cultureInfo) {
+			if (null == cultureInfo) return;
+			if (cultureInfo.Equals(Labels.Culture)) return;
+			Labels.Culture = cultureInfo;
+			Thread.CurrentThread.CurrentUICulture = cultureInfo;
+			Thread.CurrentThread.CurrentCulture = cultureInfo;
+			RaisePropertyChanged("");
+		}
+
+	}
+}
