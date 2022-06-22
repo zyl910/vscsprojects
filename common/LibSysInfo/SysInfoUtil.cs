8841243c52c21b94e11bce0f7371bb42e8c17850
zyl910 <zyl910@sina.com>
2022-06-22 10:16:58 +0800

    ConsoleAppNet5UseStd20: 将.NET Standard版本由2.1转为2.0, 并引用 LibSysInfo

---
+++ common/LibSysInfo/SysInfoUtil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibSysInfo {
+    /// <summary>
+    /// System info util.
+    /// </summary>
+    public class SysInfoUtil {
+    }
+}
