230d0660ebf5e8062a8b56901b307d8e148e6530
zyl910 <zyl910@sina.com>
2022-07-02 17:56:35 +0800

    TestImmutable引用了nuget的库后，变为使用nuget的ImmutableList，而不再是.NET Core自带的库了.

--- vs15_2017/NCore/TestImmutable/TestImmutable/Program.cs
+++ vs15_2017/NCore/TestImmutable/TestImmutable/Program.cs
@@ -2,19 +2,33 @@
 using System.Collections.Immutable;
 using System.IO;
 using System.Reflection;
+using TestImmutableLib;
 
 namespace TestImmutable {
     class Program {
+
+        /// <summary>
+        /// Output object.
+        /// </summary>
+        /// <typeparam name="T">The object type</typeparam>
+        /// <param name="tw">Output TextWriter.</param>
+        /// <param name="obj">The object.</param>
+        static void OutputObject<T>(TextWriter tw, T obj) {
+            Assembly assembly = obj.GetType().GetTypeInfo().Assembly;
+            tw.WriteLine(string.Format("list Type.Assembly:\t{0}", assembly));
+            tw.WriteLine(string.Format("list Type.Assembly.CodeBase:\t{0}", assembly.CodeBase));
+            tw.WriteLine(string.Format("list Type.FullName:\t{0}", obj.GetType().FullName));
+            tw.WriteLine(string.Format("list toString:\t{0}", obj));
+        }
+
         static void Main(string[] args) {
             TextWriter tw = Console.Out;
             tw.WriteLine("TestImmutable");
             tw.WriteLine();
             ImmutableList<int> list = ImmutableList<int>.Empty.Add(10).Add(20).Add(30);
-            Assembly assembly = list.GetType().GetTypeInfo().Assembly;
-            tw.WriteLine(string.Format("list Type.Assembly:\t{0}", assembly));
-            tw.WriteLine(string.Format("list Type.Assembly.CodeBase:\t{0}", assembly.CodeBase));
-            tw.WriteLine(string.Format("list Type.FullName:\t{0}", list.GetType().FullName));
-            tw.WriteLine(string.Format("list toString:\t{0}", list));
+            OutputObject(tw, list);
+            ImmutableList<int> list2 = ImmutableLibUtil.MakeImmutable(tw);
+            OutputObject(tw, list2);
         }
     }
 }
---
+++ vs15_2017/NCore/TestImmutable/TestImmutableLib/ImmutableLibUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection;
+
+namespace TestImmutableLib {
+    public static class ImmutableLibUtil {
+        public static ImmutableList<int> MakeImmutable(TextWriter tw) {
+            tw.WriteLine("TestImmutable");
+            tw.WriteLine();
+            ImmutableList<int> list = ImmutableList<int>.Empty.Add(10).Add(20).Add(30);
+            Assembly assembly = list.GetType().GetTypeInfo().Assembly;
+            tw.WriteLine(string.Format("InLib: Type.Assembly:\t{0}", assembly));
+            tw.WriteLine(string.Format("InLib: Type.Assembly.CodeBase:\t{0}", assembly.CodeBase));
+            return list;
+        }
+    }
+}
