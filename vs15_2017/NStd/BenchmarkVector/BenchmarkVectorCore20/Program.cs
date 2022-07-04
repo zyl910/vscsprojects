6a48501718a3c6f06646fea63af727cd38a72dcf
zyl910 <zyl910@sina.com>
2022-07-04 20:48:33 +0800

    BenchmarkVector: 写好 SumBase、SumVector4 评测函数.

--- vs15_2017/NStd/BenchmarkVector/BenchmarkVectorCore20/Program.cs
+++ vs15_2017/NStd/BenchmarkVector/BenchmarkVectorCore20/Program.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using BenchmarkVector;
+using System;
+using System.IO;
 
 namespace BenchmarkVectorCore20 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string indent = "";
+            TextWriter tw = Console.Out;
+            tw.WriteLine("BenchmarkVectorCore20");
+            tw.WriteLine();
+            BenchmarkVectorDemo.OutputEnvironment(tw, indent);
+            BenchmarkVectorDemo.Benchmark(tw, indent);
         }
     }
 }
